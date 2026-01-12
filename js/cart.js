5dd064895f0ee472fe90e805f91046f4d644f4b7
Raya Mahardika <rayapkun@gmail.com>
2026-01-12 20:20:25 +0700

    Add files via upload

--- js/cart.js
+++ js/cart.js
@@ -1,61 +1,31 @@
-// ambil data cart
 function getCart() {
   return JSON.parse(localStorage.getItem("cart")) || [];
 }
 
-// simpan cart
 function saveCart(cart) {
   localStorage.setItem("cart", JSON.stringify(cart));
 }
 
-// tambah produk
 function addToCart(product) {
   const cart = getCart();
-  const index = cart.findIndex(item => item.id === product.id);
+  const index = cart.findIndex(i => i.id === product.id);
 
   if (index !== -1) {
-    cart[index].qty += 1;
+    cart[index].qty++;
   } else {
-    cart.push({
-      id: product.id,
-      name: product.name,
-      price: product.price,
-      image: product.image,
-      qty: 1
-    });
+    cart.push({ ...product, qty: 1 });
   }
 
   saveCart(cart);
-  alert("Produk masuk ke keranjang");
+  alert("Ditambahkan ke keranjang");
 }
 
-// hapus produk
 function removeFromCart(id) {
-  const cart = getCart().filter(item => item.id !== id);
+  const cart = getCart().filter(i => i.id !== id);
   saveCart(cart);
-  renderCart();
+  location.reload();
 }
 
-// update qty
-function updateQty(id, qty) {
-  const cart = getCart();
-
-  const item = cart.find(i => i.id === id);
-  if (!item) return;
-
-  if (qty <= 0) {
-    removeFromCart(id);
-    return;
-  }
-
-  item.qty = qty;
-  saveCart(cart);
-  renderCart();
-}
-
-// hitung total
 function getTotal() {
-  return getCart().reduce((total, item) => {
-    return total + item.price * item.qty;
-  }, 0);
+  return getCart().reduce((sum, i) => sum + i.price * i.qty, 0);
 }
--- js/data.js
+++ js/data.js
@@ -4,13 +4,13 @@ const products = [
     name: "Ayam Geprek",
     price: 16000,
     rating: 4.9,
-    image: "assets/images/ayam-geprek.avif"
+    image: "./assets/images/ayam-geprek.avif"
   },
   {
     id: 2,
     name: "Es Teh Manis",
     price: 5000,
     rating: 4.9,
-    image: "assets/images/esteh.webp"
+    image: "./assets/images/esteh.webp"
   }
 ];
